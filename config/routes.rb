904a3386ffc39bdfc81d8de6ccfefe434f4cc167
YungKeeks <clrendon92@gmail.com>
2023-01-17 21:58:18 +0000

    Added liked page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
 
-  get "/:username" => "users#show"
+  get "/:username/liked" => "photos#liked", as: :liked_photos
+
+  get "/:username" => "users#show", as: :user
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
