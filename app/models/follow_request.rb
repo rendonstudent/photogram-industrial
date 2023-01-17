df30b428d93aab15dfcf84a10407b3a04fd86c39
YungKeeks <clrendon92@gmail.com>
2023-01-17 18:57:14 +0000

    Addeddefault to private maybe

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -23,6 +23,6 @@ class FollowRequest < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :sender, class_name: "User"
 
-  enum status: { pending: "Pending", rejected: "Rejected", accepted: "Accepted" }
+  enum status: { pending: "pending", rejected: "rejected", accepted: "accepted" }
 
 end
